92219ca21e0b86b7c8457f888050c3fd9ba22318
Jessi2205 <147417230+Jessi2205@users.noreply.github.com>
2023-11-04 21:21:24 +0530

    #include<stdio.h> int main() {     int n,ds=0;     scanf("%d",&n);     while (n>0)     {         ds=ds+n%10;         n=n/10;     }     printf("%d",ds); }

--- Sum_of_all_digits.c
+++ Sum_of_all_digits.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int n,r,ds=0;
-	scanf("%d",&n);
-	while (n>0)
-	{
-		r=n%10;
-		ds+=r;
-		n=n/10;
-	}
-	printf("%d",ds);
+    int n,ds=0;
+    scanf("%d",&n);
+    while (n>0)
+    {
+        ds=ds+n%10;
+        n=n/10;
+    }
+    printf("%d",ds);
 }
