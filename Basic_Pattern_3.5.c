82a0e7acbc845e5843f411e7860dd54abb72307e
Jessi2205 <147417230+Jessi2205@users.noreply.github.com>
2023-11-27 21:18:48 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for (j=n;j>=1;j--)     {         for (i=1;i<=n;i++)         {             if (i<=j)             {                 printf("%c ",i+64);             }         }         printf(" ");     } }

---
+++ Basic_Pattern_3.5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for (j=n;j>=1;j--)
+    {
+        for (i=1;i<=n;i++)
+        {
+            if (i<=j)
+            {
+                printf("%c ",i+64);
+            }
+        }
+        printf("
+");
+    }
+}
