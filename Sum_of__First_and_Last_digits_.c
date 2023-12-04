93c124e7784ea02be988b1006e32e36aa48d5bb8
Jessi2205 <147417230+Jessi2205@users.noreply.github.com>
2023-12-04 21:08:51 +0530

    #include<stdio.h> int main() {     int n,sum=1;     scanf("%d",&n);     printf("%d",sum+n%10); }

--- Sum_of__First_and_Last_digits_.c
+++ Sum_of__First_and_Last_digits_.c
@@ -1,16 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=1;
+    int n,sum=1;
     scanf("%d",&n);
-    for (i=1;i<=n;i++)
-    {
-        if (n%10==0)
-        {
-            sum+=n;
-            while (n>0)
-            break;
-        }
-    }
     printf("%d",sum+n%10);
 }
