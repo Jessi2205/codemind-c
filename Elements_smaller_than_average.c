6986cd38798787a8ee70b1883cc84db14e34a7df
Jessi2205 <147417230+Jessi2205@users.noreply.github.com>
2023-12-15 22:33:33 +0530

    #include<stdio.h> int main() {     int n,sum=0,ave,b=0;     scanf("%d",&n);     int arr[n];     for(int i = 0 ; i < n ; i++)     {         scanf("%d",&arr[i]);         sum = sum+arr[i];     }     ave = sum/n;     for(int i = 0 ; i < n ; i++)     {         if(ave>=arr[i])         {             b++;         }     }     printf("%d",b); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,sum=0,ave,b=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i = 0 ; i < n ; i++)
+    {
+        scanf("%d",&arr[i]);
+        sum = sum+arr[i];
+    }
+    ave = sum/n;
+    for(int i = 0 ; i < n ; i++)
+    {
+        if(ave>=arr[i])
+        {
+            b++;
+        }
+    }
+    printf("%d",b);
+}
