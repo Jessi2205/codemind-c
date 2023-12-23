a09b1d9d93fe6b2f9c1e890403652321f8817e17
Jessi2205 <147417230+Jessi2205@users.noreply.github.com>
2023-12-23 22:45:59 +0530

    #include <stdio.h> int main() {
    	int n, i, j; 
    	scanf("%d", &n);
    	int arr[n];
    	for (i = 0; i < n; i++) {
    
    		scanf("%d", &arr[i]);
    	}
    	int flag = 0;
    	for (i = 0; i < n; i++)
    	{
    		int occ = 0;
    		int visited = 0;
    		for (j = 0; j < n; j++) {
    
    			if (j < i && arr[i] == arr[j]) {
    
    				visited = 1;
    				break;
    
    			}
    
    			if (arr[i] == arr[j]) occ++;
    
    		}
    
    		if (visited == 0 && arr[i] == occ) {
    
    			printf("%d ", arr[i]);
    
    			flag = 1;
    
    		}
    
    	}
    
    	if (flag == 0) {
    
    	    printf("-1");
    
    	}
    
    }

---
+++ Elements_same_as_count.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main() {
+
+	int n, i, j; 
+	scanf("%d", &n);
+	int arr[n];
+	for (i = 0; i < n; i++) {
+
+		scanf("%d", &arr[i]);
+	}
+	int flag = 0;
+	for (i = 0; i < n; i++)
+	{
+		int occ = 0;
+		int visited = 0;
+		for (j = 0; j < n; j++) {
+
+			if (j < i && arr[i] == arr[j]) {
+
+				visited = 1;
+				break;
+
+			}
+
+			if (arr[i] == arr[j]) occ++;
+
+		}
+
+		if (visited == 0 && arr[i] == occ) {
+
+			printf("%d ", arr[i]);
+
+			flag = 1;
+
+		}
+
+	}
+
+	if (flag == 0) {
+
+	    printf("-1");
+
+	}
+
+}
