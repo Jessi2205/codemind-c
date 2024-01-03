7cbd7c79af195b0bc9ddcb29da17a99cca3d43a1
Jessi2205 <147417230+Jessi2205@users.noreply.github.com>
2024-01-03 07:56:25 +0530

    #include<stdio.h> int main() {     char S[100];     scanf("%[^ ]",&S);     int count=0;     for (int i=0;S[i]!=NULL;i++)     {         if (S[i]>=65 && S[i]<=90)         {             count++;         }     }     printf("%d",count); }

---
+++ Uppercase_Letters_in_a_String.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char S[100];
+    scanf("%[^
+]",&S);
+    int count=0;
+    for (int i=0;S[i]!=NULL;i++)
+    {
+        if (S[i]>=65 && S[i]<=90)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
